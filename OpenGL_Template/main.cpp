313e1f66d7b008693dc3ae234c642a62519e7ec2
Aleksandras <s.aleksandras@gmail.com>
2017-02-18 23:01:20 +0200

    Init

---
+++ OpenGL_Template/main.cpp
@@ -0,0 +1,108 @@
+#include <SDL.h>
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/matrix.hpp>
+
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include "main.h"
+
+static SDL_Window*		st_window = nullptr;
+static SDL_GLContext	st_opengl = nullptr;
+
+struct opengl_attr_pair
+{
+	SDL_GLattr key;
+	int value;
+};
+
+opengl_attr_pair st_config [] =
+{
+    {SDL_GL_RED_SIZE,					8},
+    {SDL_GL_GREEN_SIZE,					8},
+    {SDL_GL_BLUE_SIZE,					8},
+    {SDL_GL_ALPHA_SIZE,					8},
+    {SDL_GL_BUFFER_SIZE,				32},
+    {SDL_GL_DOUBLEBUFFER,				1},
+    {SDL_GL_DEPTH_SIZE,					24},
+    {SDL_GL_STENCIL_SIZE,				8},
+    {SDL_GL_ACCUM_RED_SIZE,				0},
+    {SDL_GL_ACCUM_GREEN_SIZE,			0},
+    {SDL_GL_ACCUM_BLUE_SIZE,			0},
+    {SDL_GL_ACCUM_ALPHA_SIZE,			0},
+    {SDL_GL_STEREO,						0},
+    {SDL_GL_MULTISAMPLEBUFFERS,			1},
+    {SDL_GL_MULTISAMPLESAMPLES,			4},
+    {SDL_GL_ACCELERATED_VISUAL,			1},
+    {SDL_GL_CONTEXT_MAJOR_VERSION,		4},
+    {SDL_GL_CONTEXT_MINOR_VERSION,		4},
+    {SDL_GL_CONTEXT_EGL,				0},
+    {SDL_GL_CONTEXT_FLAGS,				SDL_GL_CONTEXT_DEBUG_FLAG},
+    {SDL_GL_CONTEXT_PROFILE_MASK,		SDL_GL_CONTEXT_PROFILE_CORE},
+    {SDL_GL_SHARE_WITH_CURRENT_CONTEXT,	1},
+    {SDL_GL_FRAMEBUFFER_SRGB_CAPABLE,	1}
+};
+
+void init () 
+{
+#ifdef _DEBUG
+	SDL_LogSetAllPriority (SDL_LOG_PRIORITY_VERBOSE);
+#endif
+	SDL_Init (SDL_INIT_EVERYTHING);
+	std::atexit([] ()
+	{
+		if (st_window) SDL_DestroyWindow(st_window);
+		if (st_opengl) SDL_GL_DeleteContext(st_opengl);
+		SDL_Quit();
+	});
+
+	for(const auto& it : st_config)
+	{
+		SDL_GL_SetAttribute(it.key, it.value);
+	}
+
+	st_window = SDL_CreateWindow (nullptr, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280u, 720u, SDL_WINDOW_OPENGL);
+	assert (st_window != nullptr);
+	st_opengl = SDL_GL_CreateContext (st_window);
+	assert (st_opengl != nullptr);
+
+	glewExperimental = true;
+	auto loc_glewok = glewInit ();
+	assert (loc_glewok == GLEW_OK);
+}
+
+void finish_frame () 
+{
+	SDL_GL_SwapWindow (st_window);
+}
+
+void render_frame () 
+{
+	glClearColor (1.0f, 0.0f, 1.0f, 1.0f);
+	glClear (GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+}
+
+bool poll_events () 
+{
+	SDL_Event loc_event;
+	while (SDL_PollEvent (&loc_event)) {
+		if (loc_event.type == SDL_QUIT) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main(int, char**)
+{
+	init ();
+
+	while (poll_events ())
+	{
+		render_frame ();	
+		finish_frame ();
+	}
+
+	return 0;
+}
