3c4fe878e17a3aa4b2e398839121faed48374029
Kev <Kev@KEV-PC>
2017-07-09 23:09:12 +0100

    Was working before I tried to combine everything into one buffer

--- OpenGL_Template/Buffer.cpp
+++ OpenGL_Template/Buffer.cpp
@@ -6,41 +6,48 @@
 static constexpr auto POSITION_ATTR = 0u;
 static constexpr auto COLOR_ATTR = 1u;
 static constexpr auto NORMAL_ATTR = 2u;
+static constexpr auto MODEL_ATTR = 4u;
+static constexpr auto WORLD_ATTR = 8u;
 
-Buffer::Buffer(std::uint32_t target_, std::size_t initial_length_):
+Buffer::Buffer(std::uint32_t target_, std::size_t initial_length_) :
 	m_target(target_),
-	m_length(initial_length_)
+	m_length(initial_length_),
+	m_vertexBufferID(0)
+	//m_arrayBufferID(0)
 {
 }
 
 
 void Buffer::createGeoBuffer(const void* data, std::size_t size)
 {
-	glGenBuffers(1, &m_bufferID);
-	glBindBuffer(m_target, m_bufferID);
+	glGenBuffers(1, &m_vertexBufferID);
+	glBindBuffer(m_target, m_vertexBufferID);
 	glBufferData(m_target, size, nullptr, GL_DYNAMIC_DRAW);
 	glBufferSubData(m_target, 0, size, (void*)data);
 
-	std::uint32_t vertexArrayID;
-	glGenVertexArrays(1, &vertexArrayID);
-	glBindVertexArray(vertexArrayID);
+	glGenVertexArrays(1, &m_arrayID);
+	glBindVertexArray(m_arrayID);
 	glEnableVertexAttribArray(POSITION_ATTR);
 	glVertexAttribPointer(POSITION_ATTR, 3u, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
 	glEnableVertexAttribArray(COLOR_ATTR);
 	glVertexAttribPointer(COLOR_ATTR, 3u, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)sizeof(glm::tvec3<GLfloat>));
 	glEnableVertexAttribArray(NORMAL_ATTR);
 	glVertexAttribPointer(NORMAL_ATTR, 3u, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(glm::tvec3<GLfloat>) * 2));
+	const glm::mat4 identityMatrix = glm::mat4(1.0);
+	glGenBuffers(1, &m_viewMatrixBufferID);
+	createMatrixBuffer(&identityMatrix, sizeof(glm::mat4), MODEL_ATTR, m_viewMatrixBufferID);
+	glGenBuffers(1, &m_worldMatrixBufferID);
+	createMatrixBuffer(&identityMatrix, sizeof(glm::mat4), WORLD_ATTR, m_worldMatrixBufferID);
 }
 
-void Buffer::createMatrixBuffer(const void* data, std::size_t size, std::uint32_t attribute) {
-	glGenBuffers(1, &m_bufferID);
-	glBindBuffer(m_target, m_bufferID);
+void Buffer::createMatrixBuffer(const void* data, std::size_t size, std::uint32_t attribute, std::uint32_t vertexBufferID) {
+	//m_arrayID = vertexArrayID;
+	//glGenBuffers(1, &m_vertexBufferID);
+	glBindBuffer(m_target, vertexBufferID);
 	glBufferData(m_target, size, nullptr, GL_DYNAMIC_DRAW);
 	glBufferSubData(m_target, 0, size, (void*)data);
 
-	std::uint32_t vertexArrayID;
-	glGenVertexArrays(1, &vertexArrayID);
-	glBindVertexArray(vertexArrayID);
+	glBindVertexArray(m_arrayID);
 	glEnableVertexAttribArray(attribute);
 	for (int i = 0; i < 4; ++i) {
 		glEnableVertexAttribArray(attribute + i);
@@ -51,14 +58,22 @@ void Buffer::createMatrixBuffer(const void* data, std::size_t size, std::uint32_
 
 }
 
-//void Buffer::createBuffer(const void* data, std::size_t size) {
-//	return;
-//}
-//
 //void Buffer::createVAOs(const void* data, std::vector<std::uint32_t> attributes, std::uint32_t step, std::uint32_t initialOffset) {
 //	return;
 //}
 
 std::uint32_t Buffer::getBufferID() {
-	return m_bufferID;
+	return m_vertexBufferID;
+}
+
+std::uint32_t Buffer::getArrayID() {
+	return m_arrayID;
+}
+
+std::uint32_t Buffer::getViewMatrixBufferID() {
+	return m_viewMatrixBufferID;
+}
+
+std::uint32_t Buffer::getWorldMatrixBufferID() {
+	return m_worldMatrixBufferID;
 }
--- OpenGL_Template/Buffer.h
+++ OpenGL_Template/Buffer.h
@@ -8,14 +8,21 @@ struct Buffer
 {
 	Buffer(std::uint32_t target_, std::size_t initial_length_);
 	void createGeoBuffer(const void* data, std::size_t size);
-	void createMatrixBuffer(const void* data, std::size_t size, std::uint32_t attribute);
+	void createMatrixBuffer(const void* data, std::size_t size, std::uint32_t attribute, std::uint32_t vertexBufferID);
 	std::uint32_t getBufferID();
+	std::uint32_t getArrayID();
+	std::uint32_t Buffer::getViewMatrixBufferID();
+	std::uint32_t Buffer::getWorldMatrixBufferID();
 private:
 	//void createBuffer(const void* data, std::size_t size);
 	//void createVAOs(const void* data, std::vector<std::uint32_t> attributes, std::uint32_t step, std::uint32_t initialOffset);
 	std::uint32_t m_target;
 	std::size_t m_length;
 	//std::size_t m_size;
-	std::uint32_t m_bufferID;
+	std::uint32_t m_vertexBufferID;
+	std::uint32_t m_viewMatrixBufferID;
+	std::uint32_t m_worldMatrixBufferID;
+	//std::uint32_t m_arrayBufferID;
+	std::uint32_t m_arrayID;
 
 };
