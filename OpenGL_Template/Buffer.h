1135ecb40cea71c442b30b050e3448f8ff3d2017
Kev <Kev@KEV-PC>
2017-07-17 10:36:42 +0100

    Multiple matrix buffers being sent down correctly

--- OpenGL_Template/Buffer.h
+++ OpenGL_Template/Buffer.h
@@ -10,27 +10,28 @@
 
 struct Buffer
 {
+	// // Constructor // //
 	Buffer(std::uint32_t target_, std::size_t initial_length_);
-	void createGeoBuffer(const std::vector<ShapeData>& shapes);
-		//const void* vertex_data, std::size_t vertex_size,
-		//const void* indice_data, std::size_t indice_size,
-		//std::size_t indice_number);
+
+	// // Functions // //
+	void addShape(const ShapeData& shape, const glm::mat4 matrix = glm::mat4(1.0f));
+	void addShape(const ShapeData& shape, const std::vector<glm::mat4>& matrices);
+	void createGeoBuffer();
 	void createMatrixBuffer(const void* data, std::size_t size, std::uint32_t attribute, std::uint32_t vertexArrayID);
 	void drawGeo(const Camera& cam, const glm::mat4& projection);
 	std::uint32_t getBufferID();
 	std::uint32_t Buffer::getViewMatrixBufferID();
 	std::uint32_t Buffer::getWorldMatrixBufferID();
 private:
-	//void createBuffer(const void* data, std::size_t size);
 	std::uint32_t m_target;
 	std::size_t m_length;
-	//std::size_t m_size;
 	std::uint32_t m_vertexBufferID;
 	std::uint32_t m_viewMatrixBufferID;
 	std::uint32_t m_worldMatrixBufferID;
-	//std::uint32_t m_arrayBufferID;
 	std::vector<std::uint32_t> m_arrayIDs;
 	std::size_t m_bufferSize;
+	std::vector<std::size_t> m_instances;
+	std::vector<glm::mat4>   m_matrices;
 	std::vector<std::size_t> m_vertexSizes;
 	std::vector<std::size_t> m_indiceSizes;
 	std::vector<std::size_t> m_indice_numbers;
