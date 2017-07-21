d2f00f86f976460a7279149adb4e3c04515fd004
Kev <Kev@KEV-PC>
2017-07-21 16:10:50 +0100

    Constructors and assigners

--- OpenGL_Template/Buffer.h
+++ OpenGL_Template/Buffer.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
+struct Buffer
+{
+	Buffer(std::size_t);
+	~Buffer();
+	Buffer(const Buffer&) = delete;
+	Buffer(Buffer&&);
+	Buffer& operator = (const Buffer&) = delete;
+	Buffer& operator = (Buffer&&);
+private:
+	std::size_t m_size;
+
+
+};
