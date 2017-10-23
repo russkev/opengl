1d9202a5131ed69e6b8a497069c9ffe0208a5218
Kev <kev_kix@hotmail.com>
2017-10-23 07:17:50 +0100

    Cleaned up code

--- OpenGL_Template/VAO.h
+++ OpenGL_Template/VAO.h
@@ -11,16 +11,12 @@
 struct VAO
 {
 	// // BIG 6
-	VAO(/*const std::uint32_t target*/);
+	VAO();
 
 	// // GENERAL FUNCTIONS
-	void Append(const std::uint32_t attribute, const std::size_t numElements, const std::size_t elementSize, const std::uint32_t type);
 	void GenerateID(const Buffer& inBuffer);
 	void GenerateVAO(const Buffer& inBuffer, std::size_t divisor, const member_info_type* begin, const member_info_type* end, std::uint32_t id_offset =  0u);
-	void ClearVectors();
 	void Bind();
-	
-	//void BindBuffer(const Buffer& inputBuffer);
 
 	// // GETTERS
 	std::uint32_t VAO_ID() { return m_VAO_ID; }
@@ -28,8 +24,4 @@ struct VAO
 private:
 	std::uint32_t					m_target;
 	std::uint32_t					m_VAO_ID;
-	std::vector<std::uint32_t>		m_attributes;
-	std::vector<std::size_t>		m_elementAmounts;
-	std::vector<std::size_t>		m_elementSizes;
-	std::vector<std::uint32_t>		m_types;
 };
