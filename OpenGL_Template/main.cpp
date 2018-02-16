80f22fc8cf855855a4c7d5b2efd2197ba7475b39
Kevin Russell - Surface <kev_kix@hotmail.com>
2018-02-16 08:36:12 +0000

    (working) min / max value working

--- OpenGL_Template/main.cpp
+++ OpenGL_Template/main.cpp
@@ -269,7 +269,8 @@ void initGeo(ApplicationState& _State)
 	_State.indxBuffer.Append(_State.sh.depthSort(_State.cam.getPosition()));
 
 	// // TEST // //
-	auto test_min = _State.sh.minValue(3.4f, 9.6f, 2.1f, 4.4f, 3.3f, 3.4f, 3.4f, -12.9);
+	auto test_min = _State.sh.minValue(3.4f, 9.6f, 2.1f, 4.4f, 3.3f, 3.4f, 3.4f, -12.9f);
+	auto test_max = _State.sh.maxValue(3.4f, 9.6f, 2.1f, 4.4f, 3.3f, 3.4f, 3.4f, -12.9f);
 
 	// // END TEST // //
 
