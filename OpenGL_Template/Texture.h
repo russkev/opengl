65daaf259656c872f004fbc72dc4b9741c675fd3
Kevin Russell - Surface <kev_kix@hotmail.com>
2018-03-26 09:21:33 +0100

    (working) Texture struct now working properly
    Former-commit-id: d254119f39ac8d564209c1cd4df81385612c03a6

--- OpenGL_Template/Texture.h
+++ OpenGL_Template/Texture.h
@@ -5,5 +5,13 @@
 
 struct Texture
 {
-	Texture(unsigned int program_id, const char *filename, const char *gl_texturename, GLint s_minMagFilter, GLint s_tiling, GLint s_num, GLuint s_tex_id);
+	Texture(unsigned int program_id, const char *filename, const char *gl_texturename, GLint s_minMagFilter, GLint s_tiling, GLint s_texture_num);
+private:
+	void init_targa(const char *filename);
+private:
+	tga_image	m_image;
+	GLuint		m_program_id;
+	GLuint		m_tex_id;
+	GLuint		m_tex_loc;
+	GLuint		m_tex_num;
 };
