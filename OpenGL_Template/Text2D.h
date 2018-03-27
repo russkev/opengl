1056e3739d65fa6af1867677f43ab3d9e9070b71
Kevin Russell - Surface <kev_kix@hotmail.com>
2018-03-27 20:17:50 +0100

    (not working) Working on text 2d shaders and struct
    Former-commit-id: d8b8b02b0ab94a0bb8eb8bc6eeb488a407dac576

--- OpenGL_Template/Text2D.h
+++ OpenGL_Template/Text2D.h
@@ -6,19 +6,24 @@
 #include <glm/glm.hpp>
 
 #include "Texture.h"
+#include "Buffer.h"
 
 struct Text2D
 {
 	// // TYPES // //
-	typedef std::tuple< glm::vec2, glm::vec2> vertex2DType;
+	typedef std::tuple< glm::vec2, glm::vec2>	vertex2DType;
+	typedef std::vector<vertex2DType>			vertices2DType;
+
+	// // ENUMERATION // //
+	enum attr { position2D = 0, uv2D = 1 };
 
 	// // CONSTRUCTOR // //
 	Text2D(const char* s_texture_path);
 
 
-	void print(const char* s_text, int x, int y, int size);
+	void print(const char* s_text, int s_x, int s_y, int size);
 	void cleanup();
 private:
 	Texture			m_texture;
-	vertex2DType	m_vertices;
+	vertices2DType	m_vertices;
 };
