d8b8b02b0ab94a0bb8eb8bc6eeb488a407dac576
Kevin Russell - Surface <kev_kix@hotmail.com>
2018-03-27 20:17:50 +0100

    (not working) Working on text 2d shaders and struct

--- OpenGL_Template/Text2D.cpp
+++ OpenGL_Template/Text2D.cpp
@@ -1,14 +1,35 @@
 #include "Text2D.h"
 
 // // CONSTRUCTOR
-Text2D::Text2D(const char* s_texture_path) 
-{
-	m_texture = Texture(s_texture_path);
-}
+Text2D::Text2D(const char* s_texture_path):
+	m_texture(Texture(s_texture_path))
+{}
 
-void Text2D::print(const char* s_text, int x, int y, int size)
+void Text2D::print(const char* s_text, int s_x, int s_y, int s_size)
 {
-	//for (unsigned int i = 0;)
+	unsigned int offset = 32;
+	unsigned int length = strlen(s_text);
+	for (unsigned int i = 0; i < length; ++i)
+	{
+		char character = s_text[i] - offset;
+		float uv_size = 1.0f / 16.0f;
+		float uv_x = (character % 16) * uv_size;
+		float uv_y = (character / 16) * uv_size;
+		
+		vertex2DType top_left		= { { s_x + i * s_size,				s_y + s_size	}, { uv_x,				1.0 - uv_y				} };
+		vertex2DType top_right		= { { s_x + i * s_size + s_size,	s_y + s_size	}, { uv_x + uv_size,	1.0 - uv_y				} };
+		vertex2DType bottom_left	= { { s_x + i * s_size,				s_y				}, { uv_x,				1.0 - uv_y -  uv_size	} };
+		vertex2DType bottom_right	= { { s_x + i * s_size + s_size,	s_y				}, { uv_x + uv_size,	1.0 - uv_y -  uv_size	} };
+
+		m_vertices.push_back(top_left);
+		m_vertices.push_back(bottom_left);
+		m_vertices.push_back(top_right);
+
+		m_vertices.push_back(top_right);
+		m_vertices.push_back(bottom_left);
+		m_vertices.push_back(bottom_right);
+	}
+	
 }
 void Text2D::cleanup()
 {
