6ce15fee818ffb7feef0dc353049fade430f8738
Kevin Russell - Surface <kev_kix@hotmail.com>
2019-11-22 16:52:07 +1000

    OBJ Loader updated again

--- OpenGL_Template/OBJ_Loader.h
+++ OpenGL_Template/OBJ_Loader.h
@@ -1,8 +1,14 @@
 #include "ShapeData.h"
 #include <glm/matrix.hpp>
+#include <cassert>
+
+
 
 struct OBJ_Loader
 {
+	// Set constants
+	const static float numnum;
+	//static glm::vec3 DEFAULT_COLOUR;
 	bool load_obj(const char *path);
 
 };
