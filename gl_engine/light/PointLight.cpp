ccb226e08f25a4630d0de21ec5fe632e8e0db2e7
Kevin Russell - Surface <kev_kix@hotmail.com>
2019-12-01 11:04:54 +1100

    Updated point light
    Former-commit-id: b4390a32f89e09a603d16174a146650d73129caf

--- gl_engine/light/PointLight.cpp
+++ gl_engine/light/PointLight.cpp
@@ -1,8 +1,57 @@
+#include <algorithm>
+
+#include <glm/gtx/transform.hpp>
+
 #include "PointLight.h"
 
 
+// // ----- CONSTRUCTOR ----- // //
+// Constructor
 Light::Light(const float brightness, const float radius, const glm::vec3& position) :
-	m_brightness(brightness), m_radius(radius), m_position(position)
+	m_brightness(brightness > 0? brightness : 0), 
+	m_radius(radius > 0? radius : 0), 
+	m_position(position),
+	m_lightMesh(Sphere::createSphere(radius))
+{}
+
+
+// // ----- GETTERS ----- // //
+const float& Light::brightness() const
 {
-	m_lightMesh = Sphere::createSphere(m_radius);
+	return m_brightness;
+}
+
+const float& Light::radius() const
+{
+	return m_brightness;
+}
+
+const glm::vec3& Light::position() const
+{
+	return m_position;
+}
+const glm::mat4& Light::transformMatrix() const
+{
+	return glm::translate(glm::mat4(), m_position);
+}
+
+// // ----- SETTERS ----- // //
+float Light::brightness()
+{
+	return m_brightness;
+}
+
+float Light::radius()
+{
+	return m_radius;
+}
+
+glm::vec3& Light::position()
+{
+	return m_position;
+}
+
+void Light::transform(const glm::mat4& transformMatrix)
+{
+	m_position = { transformMatrix[3][0], transformMatrix[3][1], transformMatrix[3][2] };
 }
--- gl_engine/light/PointLight.h
+++ gl_engine/light/PointLight.h
@@ -2,6 +2,7 @@
 
 #include <glm/glm.hpp>
 
+
 #include "../mesh/Sphere.h"
 
 /*
@@ -12,18 +13,20 @@
 
 struct Light
 {
-	Light(const float brightness, const float radius, const glm::vec3& position);
+	// // ----- CONSTRUCTOR ----- // //
+	Light(const float brightness, const float radius, const glm::vec3& position); 
 
 	// // ----- GETTERS ----- // //
 	const float& brightness() const;
 	const float& radius() const;
 	const glm::vec3& position() const;
-	const glm::vec4& transformMatrix() const;
+	const glm::mat4& transformMatrix() const;
 
 	// // ----- SETTERS ----- // //
 	float brightness();
 	float radius();
 	glm::vec3& position();
+	void transform(const glm::mat4&);
 
 
 private:
