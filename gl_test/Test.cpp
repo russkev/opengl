7049b54033113feb559c7beb26eacab555115031
Kevin Russell <kev_kix@hotmail.com>
2019-12-02 16:52:51 +1100

    Boost test working successfully
    Former-commit-id: bc325a2f9db854c2d1fcf56e669a8d4f2c9e7aba

---
+++ gl_test/Test.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+#define BOOST_TEST_MODULE mytests
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(myTestCase)
+{
+  BOOST_TEST(1 == 1);
+  BOOST_TEST(true);
+}
--- test/Test.cpp
+++ test/Test.cpp
@@ -1,11 +1,25 @@
-#define BOOST_TEST_MODULE mytests
+//#include "../utils/VectorUtils.h"
+//
+//#define BOOST_TEST_MODULE mytests
+//#include <boost/test/included/unit_test.hpp>
+//
+////BOOST_AUTO_TEST_CASE(myTestCase)
+////{
+////  BOOST_TEST(VectorUtils::minValue(1, 2) == 1);
+////  BOOST_TEST(true);
+////}
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/minimal.hpp>
+#include "../utils/VectorUtils.h"
 
-#include <boost/test/included/unit_test.hpp>
-#include <boost/test/unit_test.hpp>
-
-#include <boost>
-BOOST_AUTO_TEST_CASE(myTestCase)
+static void test_min()
 {
-  BOOST_TEST(1 == 1);
-  BOOST_TEST(true);
+	auto out = VectorUtils::minValue(1, 2);
+	BOOST_REQUIRE(out == 1);
+}
+
+int test_main(int argc, char* argv[])
+{
+	test_min();
+	return 0;
 }
