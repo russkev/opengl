15ce005c37f8ded5dfca7f18d9c923a9307fe027
Kevin Russell <kev_kix@hotmail.com>
2020-01-19 22:46:08 +1100

    Debugging gaussian blur struct
    Former-commit-id: 6c540eda5377cf452f143f4267e1c5fa80f37443

--- gl_engine/post/Bloom.cpp
+++ gl_engine/post/Bloom.cpp
@@ -27,37 +27,61 @@ namespace gl_engine
 	// // ----- GENERAL METHODS ----- // //
 	void Bloom::draw()
 	{
-		m_material.set_uniform("is_horizontal", true);
-		glViewport(0, 0, 800, 600);
+		//m_material.set_uniform("is_horizontal", true);
+		//glViewport(0, 0, 800, 600);
 
-		m_material.update_texture_id("image", m_tone_map->bright()->id());
-		m_mesh_node.draw();
-
+		//m_material.update_texture_id("image", m_tone_map->bright()->id());
+		//m_mesh_node.draw();
+		gaussian_blur();
 	}
 
 	void Bloom::gaussian_blur()
 	{
 		//bool is_horizontal = true, first_iteration = true;
-		//GLuint bright_id = m_tone_map->bright()->id();
-		//GLuint texture_id = bright_id;
+		//GLuint current_texture_id = m_tone_map->bright()->id();
+		////GLuint texture_id = bright_id;
 
-		//m_material.use();
+		////m_material.use();
 		//for (int i = 0; i < PASSES; ++i)
 		//{
 		//	m_pingpong_fbos[is_horizontal].bind();
 		//	m_material.set_uniform("is_horizontal", is_horizontal);
-		//	glBindTexture(GL_TEXTURE_2D, texture_id);
-		//	m_material.set_texture("image", )
+		//	m_material.update_texture_id("image", current_texture_id);
 		//	m_mesh_node.draw();
-		//	texture_id = (GLuint)is_horizontal;
+		////	glBindTexture(GL_TEXTURE_2D, texture_id);
+		////	m_material.set_texture("image", )
+		////	m_mesh_node.draw();
+		//	current_texture_id = (GLuint)is_horizontal;
 		//	is_horizontal = !is_horizontal;
 
-		//	//if (first_iteration)
-		//	//{
-		//	//	first_iteration = false;
-		//	//}
+		////	//if (first_iteration)
+		////	//{
+		////	//	first_iteration = false;
+		////	//}
 		//}
 		//m_pingpong_fbos[0].unbind();
 		//m_mesh_node.draw();
+
+
+
+		bool horizontal = true, first_iteration = true;
+		int amount = 10;
+		m_material.use();
+		for (GLuint i = 0; i < amount; ++i)
+		{
+			glBindFramebuffer(GL_FRAMEBUFFER, m_pingpong_fbos[horizontal].id());
+			m_material.set_uniform("is_horizontal", horizontal);
+			glBindTexture(
+				GL_TEXTURE_2D, first_iteration ? m_tone_map->bright()->id() : m_pingpong_textures[!horizontal].id()
+			);
+			m_mesh_node.draw();
+			horizontal = !horizontal;
+			if (first_iteration)
+			{
+				first_iteration = false;
+			}
+		}
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		m_mesh_node.draw();
 	}
 }
