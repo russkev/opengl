9f469d741d9565ed58c8a564bf55c2f5b7aa65cf
Kevin Russell - Surface <kev_kix@hotmail.com>
2020-01-22 20:07:27 +1100

    Refactored framebuffer for improved functionality
    Former-commit-id: b27912aa3f06caef7d6455007f4c025bc50cc436

--- gl_engine/shading/Framebuffer.h
+++ gl_engine/shading/Framebuffer.h
@@ -2,6 +2,7 @@
 #define GL_ENGINE_SHADING_FRAMEBUFFER_H
 
 #include <vector>
+#include <map>
 
 #include <GL/glew.h>
 
@@ -25,15 +26,23 @@ namespace gl_engine
 		bool check_bound_framebuffer();
 		void init_color_attachments(GLuint amount, GLuint offset);
 		void init_color_attachments_for_bound_framebuffer(GLuint amount, GLuint offset);
-		void add_color_buffer_textures(const std::vector<Texture*> textures);
-
-
+	private:
+		void attach_texture_to_bound_framebuffer(const Texture* texture, const GLenum attachment);
+		void attach_single_texture(const Texture* texture, const GLenum attachment);
 
 		// // ----- GETTERS ----- // //
+	public:
 		GLuint id();
+		GLuint num_color_textures();
 
 		// // ----- SETTERS ----- // //
+	public:
 		void set_attachment(GLenum attachent);
+		void push_back_color_buffer_textures(const std::vector<const Texture*> textures);
+		void push_back_color_buffer_texture(const Texture* texture);
+		void set_color_buffer_texture(const Texture* texture, const GLuint loc);
+		void set_depth_buffer_texture(const Texture* texture);
+		void set_stencil_buffer_texture(const Texture* texture);
 
 		// // ----- MEMBER VARIABLES ----- // //
 	private:
@@ -41,9 +50,10 @@ namespace gl_engine
 		GLenum m_target = GL_FRAMEBUFFER;
 		GLenum m_attachment = GL_DEPTH_ATTACHMENT;
 		GLint m_level = 0;
-		GLuint m_num_color_buffers = 0;
-		GLuint m_num_depth_buffers = 0;
-		GLuint m_num_stencil_buffers = 0;
+		//GLuint m_num_color_buffers = 0;
+		std::vector<const Texture*> m_color_textures;
+		const Texture* m_depth_texture = NULL;
+		const Texture* m_stencil_texture = NULL;
 	};
 
 } // namespace gl_engine
