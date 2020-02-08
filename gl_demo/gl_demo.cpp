fe0e869433d3c38fd5473afe515143f5c73dd8fe
Kevin Russell - Surface <kev_kix@hotmail.com>
2020-02-09 10:11:38 +1100

    Added specular demo and parallax demo

--- gl_demo/gl_demo.cpp
+++ gl_demo/gl_demo.cpp
@@ -1,9 +1,14 @@
 #define SDL_MAIN_HANDLED
 
+#include "scenes/ParallaxDemo.h"
+#include "scenes/SpecularityDemo.h"
 #include "scenes/DeferredRenderDemo.h"
+
+
 int main()
 {
-	glemo::deferred_demo();
-
+	//gl_demo::parallax_demo();
+	gl_demo::specularity_demo();
+	//gl_demo::deferred_demo();
 	return 0;
 }
