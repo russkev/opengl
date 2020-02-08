36271c7aaf12ee681561fee945618f408fba1722
Kevin Russell <kev_kix@hotmail.com>
2020-02-08 14:12:19 +1100

    Basic library working
    Former-commit-id: 44200885c5079c6365a564d1df1e8b0983866e14

---
+++ gl_engine/MathsFuncs.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+
+#include "MathsFuncs.h"
+
+
+namespace MathFuncs
+{
+	double MyMathFuncs::Add(double a, double b)
+	{
+		return a + b;
+	}
+
+	double MyMathFuncs::Subtract(double a, double b)
+	{
+		return a - b;
+	}
+
+	double MyMathFuncs::Multiply(double a, double b)
+	{
+		return a * b;
+	}
+
+	double MyMathFuncs::Divide(double a, double b)
+	{
+		return a / b;
+	}
+}
